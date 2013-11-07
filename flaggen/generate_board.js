2c3616755206934071ba093a976bb8ca3f32bc06
Sabrina Shemet <sqshemet@ender.(none)>
2013-11-07 14:18:52 -0500

    Added outline

---
+++ flaggen/generate_board.js
@@ -0,0 +1,7 @@
+//Generate 17(?) random points
+//Ideally use a seed such that specific boards are recoverable
+//Compute Voronoi regions using Euclidean distance
+//Create instance of polyclass for each region
+//See source of www.contrib.andrew.cmu.edu/~saagars/ for Voronoi guidance
+//(I can't actually do this yet, I realized I don't understand the current
+// framework well enough to work in it (polyclass specifically)). Sorry, I suck.
