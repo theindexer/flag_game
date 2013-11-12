91ec093935d0c7dd2b8be089156b03643d384661
Sabrina Shemet <sqshemet@ender.(none)>
2013-11-12 11:11:15 -0500

    Added craptastic simple round timer.

---
+++ Messingaround/src/btns/timer.js
@@ -0,0 +1,38 @@
+/* 
+** Round timer
+*/
+
+var count = 180;
+var clicked = false;
+function timer(){
+    count = count-1;
+    var minutes = Math.floor(count/60);
+    var seconds = count%60;
+    if (seconds < 10){
+	seconds = "0" + seconds;
+    }
+    document.getElementById("timer").innerHTML = "Time remaining: " + String(minutes) + ":" +String(seconds);
+
+    if (count<=0){
+	document.getElementById("timer").innerHTML = "Time's up!";
+	clearInterval(counter);
+	return;
+    }
+    }
+function startTimer(){
+
+    if (clicked === false){
+	counter = setInterval("timer()", 1000);
+	clicked = true;
+    }
+    else if (clicked === true){
+    }
+}
+function stopTimer(){
+ 
+    clearInterval(counter);
+    clicked = false;
+    count = 180;
+}
+
+			  
